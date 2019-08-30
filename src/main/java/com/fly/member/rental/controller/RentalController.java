0d4f02342da6d405de83338315627fe199c9736c
InSub Lee <dldlstjq1212@gmail.com>
2019-08-30 18:26:53 +0900

    rentalController 수정..

--- src/main/java/com/fly/member/rental/controller/RentalController.java
+++ src/main/java/com/fly/member/rental/controller/RentalController.java
@@ -1,13 +1,19 @@
-package com.fly.user.rental.controller;
+package com.fly.member.rental.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller(value = "/user/rental")
+@Controller(value = "/rental")
 public class RentalController {
+	
 	
 	private static final Logger logger = LoggerFactory.getLogger(RentalController.class);
 	
+	@RequestMapping(value = "/rental/location.do")
+	public String suchLocation() {
+		
+		return "rental/location";
+	}
 }
