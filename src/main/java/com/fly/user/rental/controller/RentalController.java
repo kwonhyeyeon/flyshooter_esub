ebca19ee25160f0408cf1eb8cebd7be694bea671
InSub Lee <dldlstjq1212@gmail.com>
2019-08-30 17:58:03 +0900

    서블릿 맵핑명 수정

---
+++ src/main/java/com/fly/user/rental/controller/RentalController.java
@@ -0,0 +1,13 @@
+package com.fly.user.rental.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller(value = "/user/rental")
+public class RentalController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(RentalController.class);
+	
+}
