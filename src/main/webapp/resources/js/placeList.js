67e71be23018b44ee8d185f6668cef54fc66506e
InSub Lee <dldlstjq1212@gmail.com>
2019-09-02 08:57:00 +0900

    대관시 지역검색 수정

---
+++ src/main/webapp/resources/js/placeList.js
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+$(document).ready(function(){
+	
+	/* 리스트 클릭시 대관신청페이지로 이동 */
+	$(".goRental").click(function(){
+		var p_num =  $(this).attr("data-num");	
+		$("#p_num").val(p_num);
+		
+		//대관신청페이지로 이동하기 위해 form추가 (id : rentalInsertForm) 
+		$("#rentalInsertForm").attr({
+			"method":"post",
+			"action":"/rental/rentalStadium.do"
+		});
+		
+		$("#rentalInsertForm").submit(); 
+	});
+	
+	
+});
