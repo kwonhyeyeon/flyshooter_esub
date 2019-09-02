ada8ecb5803281d7be098dc42f01dbe22ca7437c
HyeYeon Kwon <bluuudawn@gmail.com>
2019-09-02 15:44:06 +0900

    충돌 수정

--- src/main/java/com/fly/user/rental/controller/RentalController.java
+++ src/main/java/com/fly/user/rental/controller/RentalController.java
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-package com.fly.client.rental.controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class RentalController {
-
-	// 구장별 대관 예약 현황
-	
-	
-}
-=======
 package com.fly.user.rental.controller;
 
 import java.util.List;
@@ -29,61 +16,49 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-<<<<<<<< HEAD:src/main/java/com/fly/member/rental/controller/RentalController.java
-import com.fly.client.place.service.PlaceService;
-import com.fly.client.place.vo.PlaceVO;
-import com.fly.member.join.vo.MemberVO;
-========
 import com.fly.member.place.vo.PlaceVO;
 import com.fly.user.place.service.PlaceService;
->>>>>>>> 6d2742d12393e55baeeb257b9238b206cffccbfe:src/main/java/com/fly/user/rental/controller/RentalController.java
 
 @Controller(value = "/rental")
 public class RentalController {
-	
-	@Autowired
-	private PlaceService placeService;
-	
-	private static final Logger logger = LoggerFactory.getLogger(RentalController.class);
-	
-	@RequestMapping(value = "/rental/location.do")
-<<<<<<<< HEAD:src/main/java/com/fly/member/rental/controller/RentalController.java
-	public String searchLocation(Model model) {
-		model.addAttribute("m_id", "aaa@naver.com");
-		model.addAttribute("m_type", 0);
-========
-	public String searchLocation() {
->>>>>>>> 6d2742d12393e55baeeb257b9238b206cffccbfe:src/main/java/com/fly/user/rental/controller/RentalController.java
-		
-		return "rental/location";
-	}
-	
-	// 지역으로 검색한 구장리스트
-	@RequestMapping(value = "/rental/placeList.do", method = RequestMethod.GET)
-	public String searchPlaceList(@ModelAttribute PlaceVO pvo, Model model, RedirectAttributes redirectAttr, @RequestParam(value = "area", required = true, defaultValue = "null") String area) {
-		
-		logger.info("============="+area);
-		
-		List<PlaceVO> searchPlaceList = placeService.searchPlaceList(area);
-		
-		if(searchPlaceList.isEmpty()) {
-			redirectAttr.addFlashAttribute("message", "[" + area + "]지역에는 등록된 구장이 없습니다.");
-			return "redirect:/rental/location.do";
-		}
-		model.addAttribute("searchPlaceList", searchPlaceList);
-			for(PlaceVO list : searchPlaceList) {
-				logger.info(list.toString());
-			}
-		return "rental/placeList";
-	}
-	
-	// 대관 신청페이지
-	@RequestMapping(value = "/rental/rentalStadium.do", method = RequestMethod.POST)
-	public String rentalInsert(@ModelAttribute PlaceVO pvo, Model model) {
-		logger.info("여기옴 ?? ==============" + pvo.getP_num());
-		logger.info("==============" + pvo.toString());
-		model.addAttribute("p_num", pvo.getP_num());
-		return "rental/rentalStadium";
-	}
-	}
->>>>>>> 6d2742d12393e55baeeb257b9238b206cffccbfe
+   
+   @Autowired
+   private PlaceService placeService;
+   
+   private static final Logger logger = LoggerFactory.getLogger(RentalController.class);
+   
+   @RequestMapping(value = "/rental/location.do")
+   public String searchLocation() {
+      
+      return "rental/location";
+   }
+   
+   // 지역으로 검색한 구장리스트
+   @RequestMapping(value = "/rental/placeList.do", method = RequestMethod.GET)
+   public String searchPlaceList(@ModelAttribute PlaceVO pvo, Model model, RedirectAttributes redirectAttr, @RequestParam(value = "area", required = true, defaultValue = "null") String area) {
+      
+      logger.info("============="+area);
+      
+      List<PlaceVO> suchPlaceList = placeService.searchPlaceList(area);
+      List<PlaceVO> searchPlaceList = placeService.searchPlaceList(area);
+      
+      if(searchPlaceList.isEmpty()) {
+         redirectAttr.addFlashAttribute("message", "[" + area + "]지역에는 등록된 구장이 없습니다.");
+         return "redirect:/rental/location.do";
+      }
+      model.addAttribute("searchPlaceList", searchPlaceList);
+         for(PlaceVO list : searchPlaceList) {
+            logger.info(list.toString());
+         }
+      return "rental/placeList";
+   }
+   
+   // 대관 신청페이지
+   @RequestMapping(value = "/rental/rentalStadium.do", method = RequestMethod.POST)
+   public String rentalInsert(@ModelAttribute PlaceVO pvo, Model model) {
+      logger.info("여기옴 ?? ==============" + pvo.getP_num());
+      logger.info("==============" + pvo.toString());
+      model.addAttribute("p_num", pvo.getP_num());
+      return "rental/rentalStadium";
+   }
+   }
