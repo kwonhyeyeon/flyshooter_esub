97e17b1d610db788a3f41fd845a81c917e8adeac
InSub Lee <dldlstjq1212@gmail.com>
2019-09-05 13:20:48 +0900

    UserRental mybatis 연결

--- src/main/java/com/fly/user/rental/dao/UserRentalDao.java
+++ src/main/java/com/fly/user/rental/dao/UserRentalDao.java
@@ -1,5 +1,9 @@
 package com.fly.user.rental.dao;
 
-public interface UserRentalDao {
+import java.util.HashMap;
+import java.util.List;
 
+public interface UserRentalDao {
+	// 해당경기장에서 입력받은 날짜에 예약가능한 시간을 조회한다.
+	public List<String> searchReservationTime(HashMap<String, Object> map);
 }
--- src/main/java/com/fly/user/rental/service/UserRentalService.java
+++ src/main/java/com/fly/user/rental/service/UserRentalService.java
@@ -1,5 +1,9 @@
 package com.fly.user.rental.service;
 
+import java.util.List;
+
 public interface UserRentalService {
+	// 해당경기장에서 입력받은 날짜에 예약가능한 시간을 조회한다.
+	public List<String> searchReservationTime(String selectDay, int s_no);
 
 }
